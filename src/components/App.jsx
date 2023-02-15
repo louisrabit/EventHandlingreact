0bb2b3cb152d0e7e750f04c40986d77cb2b7ea01
Luis Manuel Brito Coelho <luiscoelho18994@gmail.com>
2023-02-15 18:01:21 +0000

    Update 13 files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function App() {
+  
+  const  [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+  
+  
+  
+  function handleClick(){
+    setHeadingText("Submited");
+  }
+  
+  function handleMouseOver(){
+    setMouseOver(true);
+  }
+  
+  function handleMouseOut(){
+    setMouseOver(false);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleClick}
+     style={{backgroundColor: isMouseOver ? "black" : "white"  }}
+     onMouseOver={handleMouseOver}
+     onMouseOut={handleMouseOut}
+      
+      >Submit</button>
     </div>
   );
 }
